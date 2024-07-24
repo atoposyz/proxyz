9abc24008a12a8098d5caa54067cd5ce0c97e10e
atoposyz <76671919+atoposyz@users.noreply.github.com>
2024-07-24 19:52:09 +0800

    debug

--- api/proxyz_server.js
+++ api/proxyz_server.js
@@ -3,10 +3,12 @@ const request = require('request');
 const app = express();
 
 app.get('/api/proxy-image', (req, res) => {
+    console.log(req)
     const imageUrl = req.query.url;  // 从查询参数中获取图片URL
     if (!imageUrl) {
         return res.status(400).send('Image URL is required');
     }
+    console.log(imageUrl)
     request(imageUrl).pipe(res);
 });
 
